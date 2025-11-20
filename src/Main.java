ba6c4221a381cc1a6712787bba7f4a6eb93094cd
michalsiudut <michal.siudut.ms@gmail.com>
2025-11-20 20:03:05 +0100

    22-24 lessons watched :d

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,20 @@
 
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Hello and welcome!");
+      double poundsValue = 200;
+      double result = poundsValue * 0.45359237;
 
-        for (int i = 1; i <= 5; i++) {
-         System.out.println("i = " + i);
-        }
+      System.out.println(result);
+
+      char firstChar = '\u003f';
+      char secondChar = '?';
+      char thirdChar = 63;
+
+      int a = -1;
+
+      System.out.println(a);
+
+      System.out.println("My chars:" + firstChar + secondChar + thirdChar);
+
     }
 }
