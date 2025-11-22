6641388ea70106128b77db5ad1ad626f23518fa1
michalsiudut <michal.siudut.ms@gmail.com>
2025-11-22 22:17:22 +0100

    33

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,8 @@
 
 public class Main {
     public static void main(String[] args) {
-      double poundsValue = 200;
-      double result = poundsValue * 0.45359237;
-
+      int result = 10;
+      result -= 9;
       System.out.println(result);
-
-      char firstChar = '\u003f';
-      char secondChar = '?';
-      char thirdChar = 63;
-
-      int a = -1;
-
-      System.out.println(a);
-
-      System.out.println("My chars:" + firstChar + secondChar + thirdChar);
-
     }
 }
